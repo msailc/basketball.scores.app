6c35f58f40364652aa15488531adaeed82f5342c
Mustafa Islamovic <mustafaislamovich@gmail.com>
2023-06-21 19:13:22 +0200

    init user interface

--- server/rest/index.php
+++ server/rest/index.php
@@ -6,14 +6,17 @@ require '../vendor/autoload.php';
 require_once __DIR__ . '/services/TeamServices.php';
 require_once __DIR__ . '/services/PlayerServices.php';
 require_once __DIR__ . '/services/MatchServices.php';
+require_once __DIR__ . '/services/StandingServices.php';
 
 Flight::register('teamService', "TeamServices");
 Flight::register('playerService', "playerServices");
 Flight::register('matchService', "matchServices");
+Flight::register('standingService', "standingServices");
 
 require_once __DIR__ . '/routes/TeamRoutes.php';
 require_once __DIR__ . '/routes/PlayerRoutes.php';
 require_once __DIR__ . '/routes/MatchRoutes.php';
+require_once __DIR__ . '/routes/StandingRoutes.php';
 
 Flight::route('GET /', function () {
     echo "Hello";
